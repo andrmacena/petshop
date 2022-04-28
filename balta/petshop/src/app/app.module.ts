1be4e8351a950cd644a8aaafd6a6d219fb341384
André Macena <andrmacena_@outlook.com>
2022-04-28 20:28:40 -0300

    Adicionado componente http para requisição ao backend

--- balta/petshop/src/app/app.module.ts
+++ balta/petshop/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -26,6 +27,7 @@ import { FramePageComponent } from './pages/master/frame.page'
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
