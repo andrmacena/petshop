980149ed94fbf62ab0b601f39f463ac4c6b90c3d
André Macena <andrmacena_@outlook.com>
2022-05-10 19:42:15 -0300

    Alteracao nos componentes de login e produtos

--- balta/petshop/src/app/pages/account/login-page/login-page.component.ts
+++ balta/petshop/src/app/pages/account/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-login-page',
@@ -6,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  public form!: FormGroup;
+
+  constructor(private service: DataService, private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      username: ['', Validators.compose([
+        Validators.minLength(11),
+        Validators.maxLength(11),
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        Validators.required
+      ])]
+    })
+
+  }
 
   ngOnInit(): void {
   }
--- balta/petshop/src/app/services/data.service.ts
+++ balta/petshop/src/app/services/data.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Product } from "../models/product.model";
 
 @Injectable({
    providedIn: 'root'
 })
 
 export class DataService {
+   public baseUrl: string = 'http://localhost:3000/v1';
    constructor(private http: HttpClient) { }
 
    getProducts() {
-      return this.http.get<any[]>('http://localhost:3000/v1/products')
+      return this.http.get<Product[]>(`${this.baseUrl}/products`)
+   }
+
+   authenticate(data: any) {
+      return this.http.post(`${this.baseUrl}/accounts/authenticate`, data)
    }
 }
