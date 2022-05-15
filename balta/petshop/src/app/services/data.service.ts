b015c40c251ab8c31fc227dc1e102319336ad545
André Macena <andrmacena_@outlook.com>
2022-05-15 18:44:20 -0300

    Adicionado novos métodos para refresh token

--- balta/petshop/src/app/services/data.service.ts
+++ balta/petshop/src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Product } from "../models/product.model";
+
 
 @Injectable({
    providedIn: 'root'
@@ -10,11 +11,23 @@ export class DataService {
    public baseUrl: string = 'http://localhost:3000/v1';
    constructor(private http: HttpClient) { }
 
+   public composeHeader() {
+      const token: any = localStorage.getItem('petshop.token');
+
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+      return headers;
+   }
+
    getProducts() {
       return this.http.get<Product[]>(`${this.baseUrl}/products`)
    }
 
    authenticate(data: any) {
-      return this.http.post(`${this.baseUrl}/accounts/authenticate`, data)
+      return this.http.post(`${this.baseUrl}/accounts/authenticate`, data);
+   }
+
+   refreshToken() {
+      return this.http.post(`${this.baseUrl}/accounts/refresh-token`, null, { headers: this.composeHeader() });
    }
 }
