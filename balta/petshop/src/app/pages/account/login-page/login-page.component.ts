93a1af3ba1aee0a29d94bf1263995648c3921772
André Macena <andrmacena_@outlook.com>
2022-05-15 18:43:01 -0300

    Modificado login page para realizar validacao do cpf

--- balta/petshop/src/app/pages/account/login-page/login-page.component.ts
+++ balta/petshop/src/app/pages/account/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DataService } from 'src/app/services/data.service';
+import { CustomValidator } from 'src/app/validators/custom.validator';
 
 @Component({
   selector: 'app-login-page',
@@ -9,14 +10,15 @@ import { DataService } from 'src/app/services/data.service';
 export class LoginPageComponent implements OnInit {
 
   public form!: FormGroup;
+  public busy: boolean = false;
 
   constructor(private service: DataService, private fb: FormBuilder) {
-
     this.form = this.fb.group({
       username: ['', Validators.compose([
-        Validators.minLength(11),
-        Validators.maxLength(11),
-        Validators.required
+        Validators.minLength(14),
+        Validators.maxLength(14),
+        Validators.required,
+        CustomValidator.isCpf()
       ])],
       password: ['', Validators.compose([
         Validators.minLength(6),
@@ -28,6 +30,25 @@ export class LoginPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const token = localStorage.getItem('petshop.token');
+
+    if (token) {
+      this.busy = true;
+      this.service.refreshToken().subscribe((data: any) => {
+        localStorage.setItem('petshop.token', JSON.stringify(data.token))
+        this.busy = false;
+      });
+    } else {
+      localStorage.clear();
+      this.busy = false;
+    }
   }
 
+  submit() {
+    this.busy = true;
+    this.service.authenticate(this.form.value).subscribe((data: any) => {
+      this.busy = false;
+      localStorage.setItem('petshop.token', JSON.stringify(data.token))
+    });
+  }
 }
