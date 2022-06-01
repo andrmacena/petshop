ebe721036495ab2745e3aefe50b12d7460c94e27
André Macena <andrmacena_@outlook.com>
2022-06-01 17:33:59 -0300

    Criado método para exibir quantidade de itens do carrinho

--- balta/petshop/src/app/components/shared/navbar/navbar.component.ts
+++ balta/petshop/src/app/components/shared/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Cart } from 'src/app/models/cart.model';
 import { User } from 'src/app/models/user.model';
+import { CartUtil } from 'src/app/utils/cart.util';
 import { Security } from 'src/app/utils/security.util';
 
 @Component({
@@ -9,11 +11,22 @@ import { Security } from 'src/app/utils/security.util';
 })
 export class NavbarComponent implements OnInit {
   public user?: User;
+  public cart?: Cart;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
     this.user = Security.getUser();
+  }
+  
+  totalItems(){
+    this.cart = CartUtil.get();
+    let total = 0;
+    this.cart?.items.forEach((cartItem) => {
+      total += cartItem.quantity
+    });
+    return total;
+
   }
 
   logout(){
