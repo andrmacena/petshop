82a9f531903d81a1f58a96d6f2d3280a279f0f5b
André Macena <andrmacena_@outlook.com>
2022-06-01 17:35:42 -0300

    alterado product card

--- balta/petshop/src/app/components/store/product-card/product-card.component.ts
+++ balta/petshop/src/app/components/store/product-card/product-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CartItem } from 'src/app/models/cart-item.model';
 import { Product } from 'src/app/models/product.model';
+import { CartUtil } from 'src/app/utils/cart.util';
 
 @Component({
   selector: 'app-product-card',
@@ -9,9 +12,21 @@ export class ProductCardComponent implements OnInit {
   //receber um objeto de outro componente
   @Input() product!: Product;
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  addToCart() {
+    const cartItem = new CartItem(
+      this.product._id,
+      this.product.title,
+      1,
+      this.product.price,
+      this.product.image);
+
+    CartUtil.add(cartItem);
+    this.toastr.success(`${this.product.title} adicionado ao carrinho`, 'Produto Adicionado');
   }
 
 }
